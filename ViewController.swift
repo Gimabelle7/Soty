4019b5bbb275eca39cc0b0071ae6b2dc2cd235a5
Gimabelle Garcia Vasquez <gimabelle01@hotmail.com>
2023-03-25 18:55:26 -0500

    Prueba

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Soty
+//
+//  Created by Gimabelle Garcia vasquez on 8/2/23.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
